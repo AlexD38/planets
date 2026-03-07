77d7c9c9799b6c5291c81d60318845b51c5e34d7
Alex <dartiguelongue.alexis@gmail.com>
2026-03-07 16:39:36 +0100

    {update} pointer icons display ok

--- src/components/Pointer.jsx
+++ src/components/Pointer.jsx
@@ -3,26 +3,37 @@ import { PlanetContext } from "../context/PlanetContext";
 import "./Pointer.css";
 
 // --- SVG Icons for Controls ---
-const RollIcon = () => <i class="fa-solid fa-angles-right"></i>;
 
 const keyDisplayMap = {
-  thrust: { name: "Thrust", icon: <i class="fa-solid fa-angles-up"></i> },
+  thrust: {
+    name: "Thrust",
+    icon: <i className="fa-solid fa-angles-up"></i>,
+    pos: "top",
+  },
   thrustReverse: {
     name: "Reverse",
-    icon: <i class="fa-solid fa-angles-down"></i>,
+    icon: <i className="fa-solid fa-angles-down"></i>,
+    pos: "bottom",
   },
-  pitchUp: { name: "Pitch ▲", icon: <i class="fa-solid fa-angle-up"></i> },
+  pitchUp: {
+    name: "Pitch ▲",
+    icon: <i className="fa-solid fa-angle-up"></i>,
+    pos: "top",
+  },
   pitchDown: {
     name: "Pitch ▼",
-    icon: <i class="fa-solid fa-angle-down"></i>,
+    icon: <i className="fa-solid fa-angle-down"></i>,
+    pos: "bottom",
   },
   rollLeft: {
     name: "Roll ◀",
-    icon: <i class="fa-solid fa-angle-left"></i>,
+    icon: <i className="fa-solid fa-angle-left"></i>,
+    pos: "left",
   },
   rollRight: {
     name: "Roll ▶",
-    icon: <i class="fa-solid fa-angle-right"></i>,
+    icon: <i className="fa-solid fa-angle-right"></i>,
+    pos: "right",
   },
 };
 
@@ -38,42 +49,41 @@ export const Pointer = () => {
     .map(([key]) => keyDisplayMap[key]);
 
   return (
-    <>
-      <div className="pointer-container">
-        <svg
-          width="100%"
-          height="100%"
-          viewBox="0 0 100 100"
-          fill="none"
-          stroke="chartreuse"
-          strokeWidth="2"
-          style={{ filter: "drop-shadow(0 0 3px chartreuse)" }}
-        >
-          {/* Central Circle */}
-          <circle cx="50" cy="50" r="3" />
+    <div className="pointer-container">
+      <svg
+        width="100%"
+        height="100%"
+        viewBox="0 0 100 100"
+        fill="none"
+        stroke="chartreuse"
+        strokeWidth="2"
+        style={{ filter: "drop-shadow(0 0 3px chartreuse)" }}
+      >
+        {/* Central Circle */}
+        <circle cx="50" cy="50" r="3" />
 
-          {/* Main crosshair lines with gap */}
-          <path d="M50 25 V0" />
-          <path d="M50 75 V100" />
-          <path d="M25 50 H0" />
-          <path d="M75 50 H100" />
+        {/* Main crosshair lines with gap */}
+        <path d="M50 25 V0" />
+        <path d="M50 75 V100" />
+        <path d="M25 50 H0" />
+        <path d="M75 50 H100" />
 
-          {/* Corner Brackets */}
-          <path d="M20 20 H30 V30" />
-          <path d="M80 20 H70 V30" />
-          <path d="M20 80 H30 V70" />
-          <path d="M80 80 H70 V70" />
-        </svg>
-      </div>
+        {/* Corner Brackets */}
+        <path d="M20 20 H30 V30" />
+        <path d="M80 20 H70 V30" />
+        <path d="M20 80 H30 V70" />
+        <path d="M80 80 H70 V70" />
+      </svg>
+
       {activeKeys.length > 0 && (
         <div className="key-display">
           {activeKeys.map((key) => (
-            <div className="key-item" key={key.name}>
+            <div className={`key-item ${key.pos}`} key={key.name}>
               {key.icon}
             </div>
           ))}
         </div>
       )}
-    </>
+    </div>
   );
 };
